2ccf998078a879297e0c2fe759dd8f396fa0e5e6
heshan <erangamx@gmail.com>
2017-05-14 21:48:59 +0530

    hidden layer nodes and learning rate changed.

--- Trainer.cpp
+++ Trainer.cpp
@@ -20,8 +20,8 @@ Trainer::~Trainer() { }
 
 int Trainer::initializeWeightMatrices(int noOfIteration) { 
     
-    classes = 25; // output node classes
-    chars = 27; // number of training chars 
+    classes = 125; // output node classes
+    chars = 125; // number of training chars 
     int w = 40, h = 40; // width x height of a char (in pixels)
     int size = 1600; // width x height
     learningRate = 0.2; // learning rate of the network 
@@ -34,19 +34,60 @@ int Trainer::initializeWeightMatrices(int noOfIteration) {
     
     
     // Initializing the input Matrix **************************************************************/
-    std::string trainingImages[] = {"imgs/training/A.jpg","imgs/training/A2.jpg","imgs/training/A3.jpg","imgs/training/B.jpg","imgs/training/C.jpg",
-                                    "imgs/training/D.jpg","imgs/training/E.jpg","imgs/training/F.jpg",
-                                    "imgs/training/G.jpg","imgs/training/H.jpg",//"imgs/training/.jpg",
-                                    "imgs/training/J.jpg","imgs/training/K.jpg","imgs/training/L.jpg",
-                                    "imgs/training/M.jpg","imgs/training/N.jpg","imgs/training/O.jpg",
-                                    "imgs/training/P.jpg","imgs/training/Q.jpg","imgs/training/R.jpg",
-                                    "imgs/training/S.jpg","imgs/training/T.jpg","imgs/training/U.jpg",
-                                    "imgs/training/V.jpg","imgs/training/W.jpg","imgs/training/X.jpg",
-                                    "imgs/training/Y.jpg","imgs/training/Z.jpg"};
+    std::string trainingImages[] = {"imgs/training/A.jpg","imgs/training/A2.jpg","imgs/training/A3.jpg","imgs/training/A4.jpg","imgs/training/A5.jpg",
+                                    "imgs/training/B.jpg","imgs/training/B2.jpg","imgs/training/B3.jpg","imgs/training/B4.jpg","imgs/training/B5.jpg",
+                                    "imgs/training/C.jpg","imgs/training/C2.jpg","imgs/training/C3.jpg","imgs/training/C4.jpg","imgs/training/C5.jpg",
+                                    "imgs/training/D.jpg","imgs/training/D2.jpg","imgs/training/D3.jpg","imgs/training/D4.jpg","imgs/training/D5.jpg",
+                                    "imgs/training/E.jpg","imgs/training/E2.jpg","imgs/training/E3.jpg","imgs/training/E4.jpg","imgs/training/E5.jpg",
+                                    "imgs/training/F.jpg","imgs/training/F2.jpg","imgs/training/F3.jpg","imgs/training/F4.jpg","imgs/training/F5.jpg",
+                                    "imgs/training/G.jpg","imgs/training/G2.jpg","imgs/training/G3.jpg","imgs/training/G4.jpg","imgs/training/G5.jpg",
+                                    "imgs/training/H.jpg","imgs/training/H2.jpg","imgs/training/H3.jpg","imgs/training/H4.jpg","imgs/training/H5.jpg",
+                                    //"imgs/training/.jpg","imgs/training/I2.jpg","imgs/training/I3.jpg","imgs/training/I4.jpg","imgs/training/I5.jpg",
+                                    "imgs/training/J.jpg","imgs/training/J2.jpg","imgs/training/J3.jpg","imgs/training/J4.jpg","imgs/training/J5.jpg",
+                                    "imgs/training/K.jpg","imgs/training/K2.jpg","imgs/training/K3.jpg","imgs/training/K4.jpg","imgs/training/K5.jpg",
+                                    "imgs/training/L.jpg","imgs/training/L2.jpg","imgs/training/L3.jpg","imgs/training/L4.jpg","imgs/training/L5.jpg",
+                                    "imgs/training/M.jpg","imgs/training/M2.jpg","imgs/training/M3.jpg","imgs/training/M4.jpg","imgs/training/M5.jpg",
+                                    "imgs/training/N.jpg","imgs/training/N2.jpg","imgs/training/N3.jpg","imgs/training/N4.jpg","imgs/training/N5.jpg",
+                                    "imgs/training/O.jpg","imgs/training/O2.jpg","imgs/training/O3.jpg","imgs/training/O4.jpg","imgs/training/O5.jpg",
+                                    "imgs/training/P.jpg","imgs/training/P2.jpg","imgs/training/P3.jpg","imgs/training/P4.jpg","imgs/training/P5.jpg",
+                                    "imgs/training/Q.jpg","imgs/training/Q2.jpg","imgs/training/Q3.jpg","imgs/training/Q4.jpg","imgs/training/Q5.jpg",
+                                    "imgs/training/R.jpg","imgs/training/R2.jpg","imgs/training/R3.jpg","imgs/training/R4.jpg","imgs/training/R5.jpg",
+                                    "imgs/training/S.jpg","imgs/training/S2.jpg","imgs/training/S3.jpg","imgs/training/S4.jpg","imgs/training/S5.jpg",
+                                    "imgs/training/T.jpg","imgs/training/T2.jpg","imgs/training/T3.jpg","imgs/training/T4.jpg","imgs/training/T5.jpg",
+                                    "imgs/training/U.jpg","imgs/training/U2.jpg","imgs/training/U3.jpg","imgs/training/U4.jpg","imgs/training/U5.jpg",
+                                    "imgs/training/V.jpg","imgs/training/V2.jpg","imgs/training/V3.jpg","imgs/training/V4.jpg","imgs/training/V5.jpg",
+                                    "imgs/training/W.jpg","imgs/training/W2.jpg","imgs/training/W3.jpg","imgs/training/W4.jpg","imgs/training/W5.jpg",
+                                    "imgs/training/X.jpg","imgs/training/X2.jpg","imgs/training/X3.jpg","imgs/training/X4.jpg","imgs/training/X5.jpg",
+                                    "imgs/training/Y.jpg","imgs/training/Y2.jpg","imgs/training/Y3.jpg","imgs/training/Y4.jpg","imgs/training/Y5.jpg",
+                                    "imgs/training/Z.jpg","imgs/training/Z2.jpg","imgs/training/Z3.jpg","imgs/training/Z4.jpg","imgs/training/Z5.jpg"
+                                };
     
-    char typeOfTrainingChars[] = {  1,  1,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 
-                                    11 ,12 ,13, 14, 15, 16, 17, 18, 19, 20,
-                                    21, 22, 23, 24, 25};
+    char typeOfTrainingChars[] = {  1, 1, 1, 1, 1,  
+                                    2, 2, 2, 2, 2,  
+                                    3, 3, 3, 3, 3,  
+                                    4, 4, 4, 4, 4,  
+                                    5, 5, 5, 5, 5,  
+                                    6, 6, 6, 6, 6,  
+                                    7, 7, 7, 7, 7,  
+                                    8, 8, 8, 8, 8,  
+                                    9, 9, 9, 9, 9,  
+                                    10,10,10,10,10, 
+                                    11,11,11,11,11,
+                                    12,12,12,12,12,
+                                    13,13,13,13,13, 
+                                    14,14,14,14,14, 
+                                    15,15,15,15,15, 
+                                    16,16,16,16,16, 
+                                    17,17,17,17,17, 
+                                    18,18,18,18,18, 
+                                    19,19,19,19,19, 
+                                    20,20,20,20,20,
+                                    21,21,21,21,21, 
+                                    22,22,22,22,22, 
+                                    23,23,23,23,23, 
+                                    24,24,24,24,24, 
+                                    25,25,25,25,25
+                                };
     
     
     char caps[] = { 'A','B','C','D','E','F','G','H',//'I',
@@ -333,11 +374,32 @@ int Trainer::printOutputLayer(){
 
 int Trainer::sortMeanList(float* list, int listSize){
     
-    char typeOfTrainingChars[] = {  1,  1,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 
-                                    11 ,12 ,13, 14, 15, 16, 17, 18, 19, 20,
-                                    21, 22, 23, 24, 25
+    char typeOfTrainingChars[] = {  1, 1, 1, 1, 1,  
+                                    2, 2, 2, 2, 2,  
+                                    3, 3, 3, 3, 3,  
+                                    4, 4, 4, 4, 4,  
+                                    5, 5, 5, 5, 5,  
+                                    6, 6, 6, 6, 6,  
+                                    7, 7, 7, 7, 7,  
+                                    8, 8, 8, 8, 8,  
+                                    9, 9, 9, 9, 9,  
+                                    10,10,10,10,10, 
+                                    11,11,11,11,11,
+                                    12,12,12,12,12,
+                                    13,13,13,13,13, 
+                                    14,14,14,14,14, 
+                                    15,15,15,15,15, 
+                                    16,16,16,16,16, 
+                                    17,17,17,17,17, 
+                                    18,18,18,18,18, 
+                                    19,19,19,19,19, 
+                                    20,20,20,20,20,
+                                    21,21,21,21,21, 
+                                    22,22,22,22,22, 
+                                    23,23,23,23,23, 
+                                    24,24,24,24,24, 
+                                    25,25,25,25,25
                                 };
-    
     
     char caps[] = { 'A','B','C','D','E','F','G','H',//'I',
                     'J','K','L','M','N','O','P','Q','R',
